d7a8c944cea11fd609ef05bd86b4a363ca653be0
Mahmoud <mahloola08@gmail.com>
2021-07-22 18:04:02 -0400

    wrote something in the about page

--- src/about/About.js
+++ src/about/About.js
@@ -1,11 +1,29 @@
-import react from 'react';
+import { Card } from 'react-bootstrap';
 
 function About() {
     return (
         <div>
-            <h1>
-                hello from about
-            </h1>
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        <h1>
+                            About
+                        </h1>
+                    </Card.Title>
+                    <Card.Subtitle>
+                        <h5>
+                        The idea of osu!collector is to allow osu! players to perform CRUD operations (Create, Read, Update, Delete) on beatmap collections.
+                        <br/>
+                        Currently there is little support for collection sharing/editing (they are encoded into one collections.db file).
+                        <br/>
+                        osu!collector is run by <a href="https://twitter.com/mahloola" style={{textDecoration: "none"}}>myself</a> (mahloola) working on the front end and <a href="https://twitter.com/FunOrange42" style={{textDecoration: "none"}}>FunOrange</a> working on the back end.
+                        <br />
+                        I sure hope we can finish this in a reasonable amount of time 😭
+                        </h5>
+                        
+                    </Card.Subtitle>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
--- src/common/Profile.js
+++ src/common/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <div>
             hiii
-            
+            {JSON.stringify(user, null, 2)}
         </div>
     )
 }
