d797fe761415e4e4bbe7d9ab808f6ee9472cc893
Mahmoud <mahloola08@gmail.com>
2021-07-22 16:30:39 -0400

    fixed footer to the bottom with inline css

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -4,18 +4,21 @@ import { Link } from "react-router-dom";
 function Footer() {
 
     return (
-        <div class="bg-dark text-light p-3">
+        // fixed to the bottom by adding inline css
+        <footer class="bg-dark text-light p-3" style={{position: "absolute", left: "0", right: "0", bottom: "0"}}>
             <Container>
                 <Row>
                     <Col>
-                    <div class="d-flex justify-content-center">
-                    © 2021 Copyright: FunOrange & mahloola •&nbsp;<Link to='/about' style={{ textDecoration: 'none' }}>About</Link>
-                    </div>                       
+                        <div class="d-flex justify-content-center">
+                            {/* nbsp because space was not working lol*/}
+                            © 2021 Copyright: FunOrange & mahloola •&nbsp;<Link to='/about' style={{ textDecoration: 'none' }}>About</Link>
+                        </div>
                     </Col>
                 </Row>
             </Container>
-        </div>
+        </footer>
     )
 }
+
 
 export default Footer;
