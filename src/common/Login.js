29b0f817c3fd5b26a488c72fa41b943bf8c78e8b
FunOrange <junarvi@yahoo.ca>
2021-07-31 16:26:43 -0400

    moved oauth callback to config

--- src/common/Login.js
+++ src/common/Login.js
@@ -1,10 +1,12 @@
 import { Form, Button } from 'react-bootstrap';
 import { Redirect, Link } from 'react-router-dom';
+import config from '../config.json'
 
 function Login() {
+    const callback = encodeURIComponent(config.OAUTH_CALLBACK)
     return (
         <>
-            <a href='https://osu.ppy.sh/oauth/authorize?client_id=7512&response_type=code&redirect_uri=https%3A%2F%2Fmap-collector-314202.uc.r.appspot.com%2Fauthentication'>
+            <a href={ `https://osu.ppy.sh/oauth/authorize?client_id=7512&response_type=code&redirect_uri=${callback}` }>
                 <Button type="submit" variant="outline-primary">Login</Button>
             </a>
         </>
