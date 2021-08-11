14c5114b44cdc4eab276821964a1d8a0e439e244
FunOrange <junarvi@yahoo.ca>
2021-08-11 14:58:23 -0400

    implemented pagination using query params + new API response

--- src/components/recent/Recent.js
+++ src/components/recent/Recent.js
@@ -2,77 +2,77 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Spinner } from 'react-bootstrap';
 import { Pagination } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import { getRecentCollections } from '../../utils/api'
+import { useQuery } from '../../utils/hooks';
 import CollectionList from '../common/CollectionList';
 
 function Recent() {
 
     const [loading, setLoading] = useState(true); // basically a flag
-    const [collections, setCollections] = useState(null);
-    const [page, setPage] = useState(1);
+    const [collectionPage, setCollectionPage] = useState(null);
+    const [page, setPage] = useState(0);
+    const query = useQuery();
+    const history = useHistory();
 
-    // run this code when on initial page load
+    // run this code on initial page load
     useEffect(() => {
-        console.log('onload')
-    })
+        setPage(query.get('page') || 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-    // run this code block when the second parameter (page) changes in value
+    // run this code when page changes
     useEffect(() => {
-        setLoading(true);
-        // GET recent collections
+        if (page <= 0)
+            return;
         getRecentCollections(page)
-            .then(data => {
+            .then(collectionPage => {
                 setLoading(false);
-                setCollections(data.collections);
-            }).catch(err =>
-                console.log('Unable to fetch collections: ', err)
-            );
+                setCollectionPage(collectionPage);
+            }).catch(err => {
+                setLoading(false);
+                console.log('Unable to fetch collections: ', err);
+            });
     }, [page])
 
-    function previousPage() {
-        if (page > 1) {
-            setPage(page - 1);
-        }
+
+    const goToPage = (page) => {
+        history.push(`/recent?page=${page}`);
+        setPage(page);
     }
 
-    function nextPage() {
-        setPage(page + 1);
-        console.log(page);
-    }
-
-    if (!loading) {
-        if (Array.isArray(collections)) {
-            return (
-                <div>
-                    <br />
-                    <CollectionList collections={collections}></CollectionList>
-                    <div className="d-flex justify-content-center">
-                        <Pagination className="text-center" size="lg">
-                            {/* <Pagination.First onClick={setPage(1)}/> */}
-                            <Pagination.Prev onClick={previousPage} />
-                            <Pagination.Item>{page}</Pagination.Item>
-                            <Pagination.Next onClick={nextPage} />
-                            {/* <Pagination.Last onClick={setPage(5)}/> */}
-                        </Pagination>
-                    </div>
-                </div>
-            )
-        }
-        else {
-            return (
-                <h1>
-                    No collections found!
-                </h1>
-            )
-        }
-    }
-    else {
+    if (loading) {
         return (
             <div className="d-flex justify-content-center">
                 <Spinner animation="border" />
             </div>
         )
     }
+    if (collectionPage && Array.isArray(collectionPage.collections)) {
+        const page = collectionPage.currentPage;
+        const nextPage = collectionPage.nextPage;
+        const lastPage = collectionPage.lastPage;
+        return (
+            <div>
+                <br />
+                <CollectionList collections={collectionPage.collections}></CollectionList>
+                <div className="d-flex justify-content-center">
+                    <Pagination className="text-center" size="lg">
+                        {page > 1 && <Pagination.First onClick={() => goToPage(1)}/>}
+                        {page > 1 && <Pagination.Prev onClick={() => goToPage(page - 1)}/>}
+                        <Pagination.Item>{page}</Pagination.Item>
+                        {nextPage && <Pagination.Next onClick={() => goToPage(nextPage)}/>}
+                        {nextPage && lastPage && <Pagination.Last onClick={() => goToPage(lastPage)}/>}
+                    </Pagination>
+                </div>
+            </div>
+        )
+    }
+    return (
+        <h1>
+            No collections found!
+        </h1>
+    )
 }
 
 export default Recent;
