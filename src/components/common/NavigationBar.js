27a553da78063670a621175c86a574c981cf5ad9
Jun Villa <jun.villa@aerlift.com>
2021-08-19 10:40:40 -0400

    started working on upload modal

--- src/components/common/NavigationBar.js
+++ src/components/common/NavigationBar.js
@@ -1,13 +1,15 @@
-import { Nav, Navbar, Form, FormControl, Button, InputGroup } from 'react-bootstrap';
+import { Nav, Navbar, Form, FormControl, Button, InputGroup, Card, Row, Col, ButtonGroup } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import UserBadge from './UserBadge';
 import Login from './Login';
+import Modal from './Modal';
 
 function NavigationBar({ user }) {
 
+    const [uploadModalIsOpen, setUploadModalIsOpen] = useState(false);
     const [searchBarInput, setSearchBarInput] = useState('');
     const history = useHistory()
 
@@ -62,7 +64,12 @@ function NavigationBar({ user }) {
                     </Form>
 
                     <div>
-                    <Button className="mx-3">Upload</Button>
+                    {process.env.NODE_ENV !== 'production' &&
+                        // TODO: check if user is logged in
+                        <Button className="mx-3" onClick={() => setUploadModalIsOpen(true)}>
+                            Upload
+                        </Button>
+                    }
 
                     {user ? <UserBadge user={user}/> : user === null ? <Login/> : null}
                     {/* 
@@ -74,6 +81,38 @@ function NavigationBar({ user }) {
                     </div>
                 </Navbar.Collapse>
             </Navbar>
+            <Modal open={uploadModalIsOpen} onClose={() => setUploadModalIsOpen(false)} >
+                <h3>1. Open <a href='https://osu.ppy.sh/wiki/en/osu%21_File_Formats/Db_%28file_format%29#collection.db'>collection.db</a></h3>
+                collection.db is a file that contains all of your osu! collections. It is located in your osu! install folder. Example: 
+                <pre className='bg-light my-2 py-1 px-3'><code>
+                    C:\Users\jun\AppData\Local\osu!\collection.db
+                </code></pre>
+                <Form.Control type="file" />
+                <br/>
+                <h3>2. Select which collections to upload</h3>
+                <div className='mb-3' style={{height: 500, overflowY: 'scroll'}}>
+                    {['speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim', 'speed', 'aim'].map(name => 
+                        <Card key={`${name}`} className='shadow-sm mx-3 my-2 py-2 px-4'>
+                            <Row>
+                                <Col>
+                                    {name}
+                                </Col>
+                                <Col>
+                                    20 beatmaps
+                                </Col>
+                                <Col xs={1}>
+                                    <Form.Check/>
+                                </Col>
+                            </Row>
+                        </Card>
+                    )}
+                </div>
+                <ButtonGroup>
+                    <Button>Select All</Button>
+                    <Button variant='secondary'>Cancel</Button>
+                    <Button>Upload</Button>
+                </ButtonGroup>
+            </Modal>
         </div>
     )
 }
