fed3267cffcef51d8f1bb857f4b6856a2a29a2a0
FunOrange <junarvi@yahoo.ca>
2021-08-19 22:43:16 -0400

    added beatmap thumbnail

--- src/components/collection/BeatmapList.js
+++ src/components/collection/BeatmapList.js
@@ -3,30 +3,35 @@ import { Button, Table } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const BeatmapList = ({ beatmaps }) => {
+
     return (
         <Table striped bordered hover>
             <thead>
                 <tr>
-                    <th>Artist - Title</th>
+                    
+                    <th> {/* beatmap thumbnail */}</th>
+                    <th>Title</th>
                     <th>Difficulty</th>
                     <th>Star Rating</th>
                     <th>Links</th>
                 </tr>
             </thead>
             <tbody>
-                {beatmaps.map(function (beatmap) {
-                    return (
-                        <tr key={beatmap.id} onClick={() => { <Redirect to="osu.ppy.sh" /> }}>
-                            <td>{beatmap.beatmapset.artist} - {beatmap.beatmapset.title}</td>
-                            <td>[{beatmap.version}]</td>
-                            <td>{beatmap.difficulty_rating}</td>
-                            <td>
-                                <Button variant='outline-secondary' size='sm' href={`https://osu.ppy.sh/b/${beatmap.id}`}>Website</Button>{' '}
-                                <Button variant='outline-primary' size='sm' href={`osu://b/${beatmap.id}`}>Direct</Button>
-                            </td>
-                        </tr>
-                    )
-                })}
+                {beatmaps.map(beatmap => 
+                    <tr key={beatmap.id} onClick={() => { <Redirect to="osu.ppy.sh" /> }}>
+                        <img
+                            src={beatmap.beatmapset.covers.card}
+                            className='p-0'
+                            style={{objectFit: 'cover', width: '100%', height: 44}}/>
+                        <td>{beatmap.beatmapset.artist} - {beatmap.beatmapset.title}</td>
+                        <td>[{beatmap.version}]</td>
+                        <td>{beatmap.difficulty_rating}</td>
+                        <td className="d-flex flex-row">
+                            <Button variant='outline-secondary' className='mx-1' size='sm' href={`https://osu.ppy.sh/b/${beatmap.id}`}>Website</Button>{' '}
+                            <Button variant='outline-primary' className='mx-1' size='sm' href={`osu://b/${beatmap.id}`}>Direct</Button>
+                        </td>
+                    </tr>
+                )}
             </tbody>
         </Table>
     )
