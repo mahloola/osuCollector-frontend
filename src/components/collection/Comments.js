1a48f1375352cfc6d7725807d5e92161d0615cd1
Jun Villa <jun.villa@mail.utoronto.ca>
2021-11-24 19:51:39 -0500

    WIP made progress on comments

--- src/components/collection/Comments.js
+++ src/components/collection/Comments.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Button, Card, Modal } from '../bootstrap-osu-collector';
-import { ChatFill } from 'react-bootstrap-icons'
+import { Button, Card, CardFooter, Modal } from '../bootstrap-osu-collector';
+import { ChatFill, HandThumbsUpFill } from 'react-bootstrap-icons'
 import styled, { css } from 'styled-components'
 import { Image } from 'react-bootstrap';
+import moment from 'moment'
 
 const CommentsPreviewCard = styled(Card)`
     &:hover {
@@ -14,23 +15,57 @@ const CommentsPreviewCard = styled(Card)`
     }
 `
 
+function Comment({ comment, user }) {
+    const relativeDate = moment.unix(comment.date._seconds).fromNow()
+
+    const likeComment = commentId => {
+        if (!user) {
+            alert('You must be logged in to like comments')
+            return
+        }
+        console.log('comment liked: ' + commentId)
+    }
+
+    return <div className='d-flex'>
+        {/* user avatar */}
+        <Image className='mr-3 mt-2 collection-card-uploader-avatar shadow-sm border' src={`https://a.ppy.sh/${comment.userId}`} roundedCircle />
+        <div className='flex-fill'>
+            {/* username & date */}
+            <small className='text-muted'> {comment.username} - {relativeDate} </small>
+            {/* comment text */}
+            <div style={{ whiteSpace: 'pre-line' }}>{comment.message}</div>
+            {/* likes */}
+            <div className='d-flex text-sm text-secondary mt-1' onClick={() => likeComment(comment.id)}>
+                <small>
+                    <HandThumbsUpFill className='mr-1' style={{marginTop: -5}}/>
+                    {comment.likes?.length || 0}
+                </small>
+            </div>
+        </div>
+    </div>
+}
+
 function Comments({ comments, user }) {
 
     const [commentsModalIsOpen, setCommentsModalIsOpen] = useState(false);
     const [unsavedComment, setUnsavedComment] = useState('')
 
-    const submitComment = () => {
-        console.log('comment submitted')
+    const postComment = () => {
+        if (!user) {
+            alert('You must be logged in to comment')
+            return
+        }
+        console.log('comment posted')
         setCommentsModalIsOpen(false)
     }
 
     return (
         <>
             <CommentsPreviewCard
-                className='p-2 mt-4 shadow'
+                className='p-0 mt-4 shadow'
                 style={{ cursor: 'pointer' }}
                 onClick={() => {
-                    if (!user) {
+                    if (!user && (!comments || comments.length === 0)) {
                         alert('You must be logged in to comment');
                         return;
                     }
@@ -38,11 +73,20 @@ function Comments({ comments, user }) {
                 }}
             >
                 {comments ?
-                    comments.slice(0, 3).map((comment, index) =>
-                        <div key={index} className='d-flex'>
-                            <Image className='collection-card-uploader-avatar shadow-sm border mr-3' src={`https://a.ppy.sh/${user.id}`} roundedCircle />
+                    <div>
+                        <div className='px-3 pt-2 pb-3'>
+                            {comments.slice(0, 3).map((comment, index) => {
+                                const last = index === comments.length - 1
+                                return <>
+                                    <Comment key={index} comment={comment} user={user} />
+                                    {!last && <hr className='my-1' />}
+                                </>
+                            })}
                         </div>
-                    )
+                        <CardFooter>
+                            <div className='text-center'>View all {comments.length} comment(s)</div>
+                        </CardFooter>
+                    </div>
                     :
                     <div className='d-flex justify-content-center align-items-center'>
                         <h3 className='text-muted mr-2'>
@@ -63,8 +107,14 @@ function Comments({ comments, user }) {
             >
                 {comments?.length > 0 &&
                     <>
-                        <div></div>
-                        <hr />
+                        {comments.slice(0, 3).map((comment, index) => {
+                            const last = index === comments.length - 1
+                            return <>
+                                <Comment key={index} comment={comment} user={user} />
+                                {last && <hr className='my-1' />}
+                            </>
+                        })}
+                        <hr className='my-4' />
                     </>
                 }
                 <div className='p-3'>
@@ -87,7 +137,7 @@ function Comments({ comments, user }) {
                             />
                             <div className='d-flex justify-content-end mb-0'>
                                 <Button
-                                    onClick={submitComment}
+                                    onClick={postComment}
                                 >
                                     <div className='d-flex justify-content-center align-items-center'>
                                         <ChatFill className='mr-2' />
