09efd1492ffa67465018def960501e16ab931c89
FunOrange <junarvi@yahoo.ca>
2022-08-03 09:39:42 -0400

    handle case where user's collection.db is empty

--- src/components/navbar/UploadModal.js
+++ src/components/navbar/UploadModal.js
@@ -57,78 +57,81 @@ function UploadModal({ uploadModalIsOpen, setUploadModalIsOpen, remoteCollection
     <Modal show={uploadModalIsOpen} onHide={() => setUploadModalIsOpen(false)} size='xl' centered>
       <ModalBody className='px-5 py-4'>
         <Form>
-          {localCollections.length > 0 && (
-            <div>
-              <h3>Select a collection to upload or update</h3>
-              <div style={{ maxHeight: '77vh', overflowY: 'scroll' }}>
-                {localCollections.map((collection, index) => (
-                  <Card $lightbg key={index} className='shadow-sm mx-2 my-2 py-2 px-4'>
-                    <div className='d-flex text-right'>
-                      <div className='flex-fill text-left'>
-                        {collection.name}
-                        <span className='text-muted ml-3'>{collection.beatmapChecksums.length} beatmaps</span>
+          <div>
+            <h3>Select a collection to upload or update</h3>
+            {localCollections.length > 0 && (
+              <>
+                <div style={{ maxHeight: '77vh', overflowY: 'scroll' }}>
+                  {localCollections.map((collection, index) => (
+                    <Card $lightbg key={index} className='shadow-sm mx-2 my-2 py-2 px-4'>
+                      <div className='d-flex text-right'>
+                        <div className='flex-fill text-left'>
+                          {collection.name}
+                          <span className='text-muted ml-3'>{collection.beatmapChecksums.length} beatmaps</span>
+                        </div>
+                        {isUploaded(collection) && (
+                          <>
+                            <span className='text-muted ml-3'>
+                              <small>
+                                last updated{' '}
+                                {moment.unix(getRemoteCollection(collection.name).dateLastModified._seconds).fromNow()}
+                              </small>
+                            </span>
+                            <Badge
+                              className='mx-3'
+                              variant='info'
+                              style={{
+                                minWidth: '50px',
+                                height: '24px',
+                                paddingTop: '6px',
+                              }}
+                            >
+                              Uploaded
+                            </Badge>
+                          </>
+                        )}
+                        <div className='ml-2'>
+                          <Form.Check
+                            checked={selectedCollection?.name === collection.name}
+                            value={index}
+                            onChange={onCheck}
+                          />
+                        </div>
                       </div>
-                      {isUploaded(collection) && (
-                        <>
-                          <span className='text-muted ml-3'>
-                            <small>
-                              last updated{' '}
-                              {moment.unix(getRemoteCollection(collection.name).dateLastModified._seconds).fromNow()}
-                            </small>
-                          </span>
-                          <Badge
-                            className='mx-3'
-                            variant='info'
-                            style={{
-                              minWidth: '50px',
-                              height: '24px',
-                              paddingTop: '6px',
-                            }}
-                          >
-                            Uploaded
-                          </Badge>
-                        </>
-                      )}
-                      <div className='ml-2'>
-                        <Form.Check
-                          checked={selectedCollection?.name === collection.name}
-                          value={index}
-                          onChange={onCheck}
+                    </Card>
+                  ))}
+                </div>
+                <br></br>
+                <div className='upload-buttons'>
+                  <Button
+                    style={{ width: '11em' }}
+                    onClick={submit}
+                    disabled={!selectedCollection}
+                    variant={selectedCollection ? 'primary' : 'outline-secondary'}
+                  >
+                    {uploading ? (
+                      <>
+                        <Spinner
+                          as='span'
+                          animation='border'
+                          size='sm'
+                          role='status'
+                          className='mx-2'
+                          aria-hidden='true'
                         />
-                      </div>
-                    </div>
-                  </Card>
-                ))}
-              </div>
-              <br></br>
-              <div className='upload-buttons'>
-                <Button
-                  style={{ width: '11em' }}
-                  onClick={submit}
-                  disabled={!selectedCollection}
-                  variant={selectedCollection ? 'primary' : 'outline-secondary'}
-                >
-                  {uploading ? (
-                    <>
-                      <Spinner
-                        as='span'
-                        animation='border'
-                        size='sm'
-                        role='status'
-                        className='mx-2'
-                        aria-hidden='true'
-                      />
-                      {isUploaded(selectedCollection) ? 'Updating...' : 'Uploading...'}
-                    </>
-                  ) : isUploaded(selectedCollection) ? (
-                    'Update'
-                  ) : (
-                    'Upload'
-                  )}
-                </Button>
-              </div>
-            </div>
-          )}
+                        {isUploaded(selectedCollection) ? 'Updating...' : 'Uploading...'}
+                      </>
+                    ) : isUploaded(selectedCollection) ? (
+                      'Update'
+                    ) : (
+                      'Upload'
+                    )}
+                  </Button>
+                </div>
+              </>
+            )}
+            {localCollections.length === 0 && <>You don&apos;t appear to have any collections...</>}
+          </div>
         </Form>
       </ModalBody>
     </Modal>
